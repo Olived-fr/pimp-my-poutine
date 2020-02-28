d35fcb4d358be14f8001f1a7cdb4b3eceb29fea3
Olived <dufour.oli@icloud.com>
2020-02-28 11:43:12 +0100

    Create EditorActivity

---
+++ app/src/main/java/com/m2dl/pimpmypoutine/Editor/Views/EditorActivity.java
@@ -0,0 +1,15 @@
+package com.m2dl.pimpmypoutine.Editor.Views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.m2dl.pimpmypoutine.R;
+
+public class EditorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+}
