1db3d0538f1d1c4ab1d24c360f999919be6357fa
Olived <dufour.oli@icloud.com>
2020-03-05 16:56:31 +0100

    WIP Editor

--- app/src/main/java/com/m2dl/pimpmypoutine/Editor/Views/EditorActivity.java
+++ app/src/main/java/com/m2dl/pimpmypoutine/Editor/Views/EditorActivity.java
@@ -1,79 +1,55 @@
 package com.m2dl.pimpmypoutine.Editor.Views;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.BitmapFactory.Options;
 import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.os.Build;
-import android.os.Bundle;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
-
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.res.ResourcesCompat;
-
 import com.m2dl.pimpmypoutine.R;
 
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
-public class EditorActivity extends AppCompatActivity {
+public class EditorActivity extends View {
+    Bitmap bitmap;
+    float x = 200;
+    float y = 200;
 
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editor);
+    public EditorActivity(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        Bitmap backgroundBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_background);
+        Drawable background = new BitmapDrawable(backgroundBitmap);
+        this.setBackground(background);
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
     }
 
-    public class TheChainView extends View {
-        Bitmap bitmap;
-        float x = 200;
-        float y = 200;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // TODO Auto-generated method stub
 
-        public TheChainView(Context context, AttributeSet attrs) {
-            super(context, attrs);
-            // TODO Auto-generated constructor stub
-            bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher_foreground);
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x = event.getX();
+                y = event.getY();
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                x = event.getX();
+                y = event.getY();
+                invalidate();
+                break;
         }
+        return true;
+    }
 
-        @Override
-        public boolean onTouchEvent(MotionEvent event) {
-            // TODO Auto-generated method stub
-
-            switch(event.getAction()){
-                case MotionEvent.ACTION_DOWN:
-                    x = event.getX();
-                    y = event.getY();
-                    Log.d("EditorActivity", String.valueOf(x));
-                    invalidate();
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    x = event.getX();
-                    y = event.getY();
-                    invalidate();
-                    break;
-            }
-            return true;
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            // TODO Auto-generated method stub
-            super.onDraw(canvas);
-            canvas.drawBitmap(bitmap, x, y, null);
-        }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // TODO Auto-generated method stub
+        super.onDraw(canvas);
+        canvas.drawBitmap(bitmap, x, y, null);
     }
 
 }
