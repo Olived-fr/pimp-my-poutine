78a337614b98e9586cb2fff083e4f8d013f98157
FlorianPERU <florian.peru@univ-tlse3.fr>
2020-03-08 21:39:27 +0100

    new editor concept

--- app/src/main/java/com/m2dl/pimpmypoutine/Editor/Views/EditorView.java
+++ app/src/main/java/com/m2dl/pimpmypoutine/Editor/Views/EditorView.java
@@ -1,22 +1,26 @@
 package com.m2dl.pimpmypoutine.Editor.Views;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LightingColorFilter;
+import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
 import com.m2dl.pimpmypoutine.R;
 
 import static com.m2dl.pimpmypoutine.Editor.Views.EditorActivity.pimpedPhoto;
 
 public class EditorView extends View {
-    Bitmap bitmap, bitmap2;
+        Bitmap bitmap, bitmap2;
     float x = 200;
     float y = 200;
     private Button buttonFiltre1;
@@ -27,14 +31,15 @@ public class EditorView extends View {
         Drawable background = new BitmapDrawable(backgroundBitmap);
         this.setBackground(background);
 
+        //bitmap = BitmapFactory.decodeFile("./app/res/mipmap-hdpi/ic_launcher.png");
         bitmap = BitmapFactory.decodeFile(pimpedPhoto);
+
         //  bitmap = BitmapFactory.decodeResource(this.getResources(), R.mipmap.ic_launcher_round);
         bitmap2 = BitmapFactory.decodeFile(pimpedPhoto);
         //  buttonFiltre1 = findViewById(R.id.buttonFiltre1);
      /*   buttonFiltre1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                buttonFiltre1.setText("oui");
+            public void onAccuracyChanged(Sensor sensor, int i) {
 
             }
         });*/
@@ -43,7 +48,6 @@ public class EditorView extends View {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         // TODO Auto-generated method stub
-
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 x = event.getX();
@@ -63,7 +67,33 @@ public class EditorView extends View {
     protected void onDraw(Canvas canvas) {
         // TODO Auto-generated method stub
         super.onDraw(canvas);
-        canvas.drawBitmap(bitmap2, 0, 0, null);
+
+       // paint.setAntiAlias(true);
+       // paint.setColor(500);
+        canvas.drawBitmap(makeTintedBitmap(bitmap2, 200), 0, 0, paint);
         canvas.drawBitmap(bitmap, x, y, null);
+    }
+    public Bitmap makeTintedBitmap(Bitmap src, int color) {
+        Bitmap result = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+        Canvas c = new Canvas(result);
+        Paint paint = new Paint();
+        paint.setColorFilter(new LightingColorFilter(color,0));
+        c.drawBitmap(src, 0, 0, paint);
+        return result;
+    }
+    public void getFiltre (String filtre) {
+       /* switch (filtre) {
+            case "filtre 1":
+                x = event.getX();
+                y = event.getY();
+                invalidate();
+                break;
+            case "filtre 2":
+                x = event.getX();
+                y = event.getY();
+                invalidate();
+                break;
+            System.out.println("OUI !!!! ");
+        }*/
     }
 }
