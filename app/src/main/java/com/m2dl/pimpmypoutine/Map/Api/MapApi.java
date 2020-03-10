9311e30f0ba8ac393164eb146c32c0711a943674
AlexandrePerge <alexandre.perge@univ-tlse3.fr>
2020-03-10 21:05:22 +0100

    feature affichage list markers

--- app/src/main/java/com/m2dl/pimpmypoutine/Map/Api/MapApi.java
+++ app/src/main/java/com/m2dl/pimpmypoutine/Map/Api/MapApi.java
@@ -1,11 +1,14 @@
 package com.m2dl.pimpmypoutine.Map.Api;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 
+import com.m2dl.pimpmypoutine.Map.Bean.DataPicture;
 import com.m2dl.pimpmypoutine.Map.Bean.MarkerIcon;
 import com.m2dl.pimpmypoutine.R;
 
@@ -19,6 +22,8 @@ import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -131,5 +136,48 @@ public class MapApi {
                 return true;
             }
         });
+    }
+
+    public void getAllPictures(){
+        //initialisation de la liste de markerIcone
+    }
+
+    public List<MarkerIcon> addMarkers(MapView myOpenMapView, Resources resources, List<DataPicture> pictures) throws IOException {
+
+        List<MarkerIcon> markerIcons = new ArrayList<>();
+
+        for(DataPicture picture : pictures) {
+            Marker marker = new Marker(myOpenMapView);
+
+            //On défini la position du marker
+            marker.setPosition(new GeoPoint(picture.getLatitude(), picture.getLongitude()));
+            marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+
+            //On défini l'image du marker
+            URL url = new URL(picture.getUrl());
+            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            Drawable image = new BitmapDrawable(resources, bmp);
+            marker.setImage(image);
+
+            //On défini l'icone du marker
+            Drawable dr = resources.getDrawable(R.drawable.maps_marker_black_icon);
+            //On transforme l'objet Drawable en Bitmap pour redéfinir sa taille
+            Bitmap bitmap = ((BitmapDrawable) dr).getBitmap();
+            Drawable icone = new BitmapDrawable(resources, Bitmap.createScaledBitmap(bitmap, 50, 50, true));
+            marker.setIcon(icone);
+
+            MarkerIcon markerIcon = new MarkerIcon();
+            markerIcon.setDrawable(icone);
+
+            setClick(myOpenMapView, marker, markerIcon, resources);
+            myOpenMapView.getOverlays().add(marker);
+
+            markerIcon.setMarker(marker);
+            addMarkerIcon(markerIcon);
+
+            markerIcons.add(markerIcon);
+        }
+        
+        return markerIcons;
     }
 }
---
+++ app/src/main/java/com/m2dl/pimpmypoutine/Map/Bean/DataPicture.java
@@ -0,0 +1,31 @@
+package com.m2dl.pimpmypoutine.Map.Bean;
+
+public class DataPicture {
+    private double latitude;
+    private double longitude;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
