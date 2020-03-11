abcd84a6a34bf3358a807ad68a16a71a1e80c964
FlorianPERU <florian.peru@univ-tlse3.fr>
2020-03-11 21:01:52 +0100

    first test for lat and long to image

--- app/src/main/java/com/m2dl/pimpmypoutine/Editor/Views/EditorActivity.java
+++ app/src/main/java/com/m2dl/pimpmypoutine/Editor/Views/EditorActivity.java
@@ -1,13 +1,22 @@
 package com.m2dl.pimpmypoutine.Editor.Views;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.media.ExifInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.m2dl.pimpmypoutine.Camera.Views.ShowPictureActivity;
 import com.m2dl.pimpmypoutine.Home.MainActivity;
 import com.m2dl.pimpmypoutine.R;
@@ -16,12 +25,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Objects;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 
 public class EditorActivity extends AppCompatActivity {
-private EditorView viewtest;
+    private EditorView viewtest;
     static String pimpedPhoto;
     private ImageButton buttonFiltre1, buttonFiltre2;
     private Button buttonValid;
@@ -39,7 +51,7 @@ private EditorView viewtest;
         buttonFiltre1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              //  buttonFiltre1.setText("oui");
+                //  buttonFiltre1.setText("oui");
                 System.out.println("fromage");
 
                 viewtest.getPoutine("fromage");
@@ -79,10 +91,84 @@ private EditorView viewtest;
                     e.printStackTrace();
                 }
                 viewtest.validImage().compress(Bitmap.CompressFormat.PNG, 100, fOut);
+              /*  ExifInterface exif = null;
+                try {
+                    exif = new ExifInterface(f.getPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                exif.setAttribute(ExifInterface.TAG_GPS_LATITUDE, String.valueOf(latLng.latitude));
+                exif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE, String.valueOf(latLng.longitude));
+*/
+                LatLng latLng = getLocation();
+
+                ExifInterface exif = null;
+                try {
+                    exif = new ExifInterface(f.getPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                exif.setAttribute(ExifInterface.TAG_GPS_LATITUDE, GPS.convert(latLng.latitude));
+                exif.setAttribute(ExifInterface.TAG_GPS_LATITUDE_REF, GPS.latitudeRef(latLng.latitude));
+                exif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE, GPS.convert(latLng.longitude));
+                exif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF, GPS.longitudeRef(latLng.longitude));
+                try {
+                    exif.saveAttributes();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    exif.saveAttributes();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    ExifInterface exif2 = new ExifInterface(f.getPath());
+                    Log.e("LATITUDE EXTRACTED", Objects.requireNonNull(exif2.getAttribute(ExifInterface.TAG_GPS_LATITUDE)));
+                    Log.e("LONGITUDE EXTRACTED", Objects.requireNonNull(exif2.getAttribute(ExifInterface.TAG_GPS_LONGITUDE)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
                 Intent mainActivity = new Intent(EditorActivity.this, MainActivity.class);
                 startActivity(mainActivity);
             }
         });
+    }
+
+    public LatLng getLocation ()
+    {
+        // Get the location manager
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria, false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    Activity#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for Activity#requestPermissions for more details.
+                //   return ;
+            }
+        }
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+        Double lat,lon;
+        try {
+            lat = location.getLatitude ();
+            // latitudeGPS = location.getLatitude ();
+            lon = location.getLongitude ();
+            // longitudeGPS = location.getLongitude ();
+            System.out.println("location.getLongitude()" + location.getLongitude());
+            System.out.println("location.getLongitude()" + location.getLatitude());
+
+            return new LatLng(lat, lon);
+        }
+        catch (NullPointerException e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
